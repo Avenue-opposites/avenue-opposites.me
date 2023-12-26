946ab7f3e82ae47decc381ab05a52d25fa9705a2
Avenue-opposites <Avenue-opposites>
2023-12-26 13:47:32 +0800

    docs: 修改consts.js注释

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+// 网站常量配置
 
-export const SITE_TITLE = 'Astro Blog'
+export const SITE_TITLE = 'Avenue Opposites'
 export const SITE_DESCRIPTION = 'Welcome to my website!'
