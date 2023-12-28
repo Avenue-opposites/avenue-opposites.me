00df758e5811eef0390291703cb3d197dce39e29
Avenue-opposites <Avenue-opposites>
2023-12-28 15:30:06 +0800

    chore: 添加自定义主题

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 import forms from '@tailwindcss/forms'
 import typography from '@tailwindcss/typography'
 import daisyui from 'daisyui'
-import theme from 'daisyui/src/theming/themes'
+import colors from 'tailwindcss/colors'
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -19,15 +19,49 @@ export default {
 	daisyui: {
 		themes: [
 			{
-				dark: {
-					...theme.dark,
-					'base-100': '#050505',
-				}
-			},
-			{
 				light: {
-					...theme.light,
-					'base-100': '#ffffff',
+					primary: colors.green[400],
+					'primary-content': colors.white,
+					secondary: colors.cyan[400],
+					'secondary-content': colors.white,
+					accent: colors.lime[400],
+					'accent-content':colors.white,
+					neutral: colors.slate[400],
+					'neutral-content':colors.white,
+					'base-100': colors.gray[50],
+					'base-200': colors.gray[100],
+					'base-300': colors.gray[200],
+					'base-content': colors.gray[900],
+					info: colors.sky[400],
+					'info-content':colors.white,
+					success: colors.green[400],
+					'success-content':colors.white,
+					warning: colors.orange[400],
+					'warning-content':colors.white,
+					error: colors.red[400],
+					'error-content':colors.white,
+				},
+				dark: {
+					primary: colors.emerald[500],
+					'primary-content':colors.white,
+					secondary: colors.cyan[500],
+					'secondary-content':colors.white,
+					accent: colors.lime[500],
+					'accent-content':colors.white,
+					neutral: colors.slate[500],
+					'neutral-content':colors.white,
+					'base-100': colors.gray[950],
+					'base-200': colors.gray[900],
+					'base-300': colors.gray[800],
+					'base-content':colors.white,
+					info: colors.sky[500],
+					'info-content':colors.white,
+					success: colors.green[500],
+					'success-content':colors.white,
+					warning: colors.orange[500],
+					'warning-content':colors.white,
+					error: colors.red[500],
+					'error-content':colors.white,
 				}
 			},
 		],
