97c768ca47db71b192aeab558813081e0cb66e33
Avenue-opposites <Avenue-opposites>
2024-01-07 01:09:23 +0800

    chore: 网站社交链接从package.json中读取

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,15 @@
+import pkg from 'package.json' assert { type: 'json' } 
+type Pkg = typeof pkg & {
+  socialLinks: {
+    github: string;
+    twitter: string;
+    bilibili: string;
+  }
+}
+const _pkg = pkg as Pkg
 // 网站常量配置
-
 export const SITE_TITLE = 'Avenue Opposites'
 export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const GITHUB_URL = _pkg.socialLinks.github
+export const TWITTER_URL = _pkg.socialLinks.twitter
+export const BILIBILI_URL = _pkg.socialLinks.bilibili
