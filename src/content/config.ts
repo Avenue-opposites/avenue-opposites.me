0f9aab656a567ed03f0de461d9cba84b467d6bec
Avenue-opposites <Avenue-opposites>
2024-01-07 01:08:21 +0800

    chore: 修改posts集合规则继承于notes集合规则

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,36 @@
-import { defineCollection, z } from 'astro:content'
+import { defineCollection, reference, z } from 'astro:content'
 
-const blog = defineCollection({
+// note frontmatter schema
+const notesModel = z.object({
+	title: z.string(),
+	description: z.string(),
+	publishDate: z.coerce.date(),
+	updatedDate: z.coerce.date().optional(),
+	heroImage: z.string().optional(),
+})
+
+const notesCollection = defineCollection({
 	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+	schema: notesModel,
+})
+
+const postsCollection = defineCollection({
+	type: 'content',
+	schema: notesModel.extend({
+		authors: reference('authors'),
 	}),
 })
 
-export const collections = { blog }
+const authorsCollection = defineCollection({
+	type: 'data',
+	schema: z.object({
+		name: z.string(),
+		avatar: z.string().optional()
+	}),
+})
+
+export const collections = { 
+	posts: postsCollection,
+	notes: notesCollection,
+	authors: authorsCollection
+}
