97ee6014e0969cc42c9f454766d187cc809a16a4
Avenue-opposites <Avenue-opposites>
2024-01-08 21:45:15 +0800

    refactor(plugin): 重构remarkShikiji插件

--- plugins/remark-shikiji.ts
+++ plugins/remark-shikiji.ts
@@ -1,14 +1,12 @@
 import type { Node } from 'unist'
-import type { BundledHighlighterOptions, BundledLanguage, BundledTheme } from 'shikiji' 
-import { getHighlighter, bundledLanguages } from 'shikiji'
+import type { 
+	BundledHighlighterOptions, 
+	BundledLanguage, 
+	BundledTheme, 
+	ShikijiTransformer 
+} from 'shikiji' 
+import { getHighlighter, bundledLanguages, bundledThemes } from 'shikiji'
 import { visit } from 'unist-util-visit'
-import { transformerTwoSlash, rendererRich } from 'shikiji-twoslash'
-import { 
-	transformerNotationDiff, 
-	transformerNotationHighlight,
-	transformerNotationFocus,
-	transformerNotationErrorLevel
-} from 'shikiji-transformers'
 
 interface CodeNode extends Node {
 	value: string;
@@ -18,30 +16,28 @@ interface CodeNode extends Node {
 
 export type Options = BundledHighlighterOptions<BundledLanguage, BundledTheme> & {
 	theme: BundledTheme,
+	highlighter?: Awaited<ReturnType<typeof getHighlighter>>,
+	transformers?: ShikijiTransformer[],
 }
 
+const bundledTheme = Object.keys(bundledThemes)
 const bundledLanguage = Object.keys(bundledLanguages)
+const defaultHighlighter = await getHighlighter({
+	themes: bundledTheme,
+	langs: bundledLanguage,
+})
 
-export default function remarkShikiji(options: Options) {	
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function remarkShikiji(options: Options = {} as any) {	
 	const {
-		theme = 'github-dark',
-		themes: _themes = {},
-		langs = [],
-		...otherOptions
-	} = options
-	let highlighter: Awaited<ReturnType<typeof getHighlighter>>
-	let loadedLangs: string[]
-	const themes: string[] = Object.keys(_themes).length ? Object.values(_themes) : [theme]
-	const themeOptions = Object.values(_themes).length ? { themes: _themes } : { theme } 
-		
-	return async (tree: Node) => {	
-		highlighter ??= await getHighlighter({ 
-			themes,
-			langs: langs.length ? langs : Object.keys(bundledLanguage), 
-			...otherOptions,
-		})
-		
-		loadedLangs ??= highlighter.getLoadedLanguages()
+		theme = 'vitesse-dark',
+		themes = {},
+		highlighter = defaultHighlighter,
+		transformers = [],
+	} = options	
+
+	return (tree: Node) => {	
+		const loadedLangs = highlighter.getLoadedLanguages()
 		// 遍历所有code类型节点
 		visit(tree, 'code', (node: CodeNode) => {
 			let lang = node?.lang || 'plaintext'
@@ -51,27 +47,15 @@ export default function remarkShikiji(options: Options) {
 				console.warn(`[remark-shikiji] ${lang} is not exist.`)
 				lang = 'plaintext'
 			}
+			
+			const themeOptions = Object.values(themes).length ? { themes } : { theme }
 
 			// 将代码转换为html语法高亮文本
 			const html = highlighter.codeToHtml(node.value, {
 				...themeOptions,
 				defaultColor: false,
 				lang,
-				transformers: [
-					transformerTwoSlash({
-						renderer: rendererRich(),
-						// 只有mete中包含twoslash才会触发
-						explicitTrigger: true,
-					}),
-					// 使用[!code ++]和[!code --]来标记添加和删除的行。
-					transformerNotationDiff(),
-					// 使用[!code highlight]来标记代码行高亮
-					transformerNotationHighlight(),
-					// 使用[!code focus]来标记焦点代码
-					transformerNotationFocus(),
-					// 使用[!code warning]和[!code error]来标记警告代码和错误代码
-					transformerNotationErrorLevel(),
-				],
+				transformers,
 				meta: {
 					// 用于twoslash解析
 					__raw: node.meta // 'twoslash',
