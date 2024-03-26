0ccfcf2f7e6f21b069d2bcfe2d9dedd42dcb6bb3
Avenue-opposites <Avenue-opposites>
2024-03-26 22:05:12 +0800

    chore: 将主题提到单独文件中

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,9 @@
 import forms from '@tailwindcss/forms'
 import typography from '@tailwindcss/typography'
 import daisyui from 'daisyui'
-import colors from 'tailwindcss/colors'
 import multiLineEllipses from './plugins/tailwindcss/multi-line-ellipses'
 import size from './plugins/tailwindcss/size'
+import theme from './theme'
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -21,54 +21,7 @@ export default {
 		size()
 	],
 	daisyui: {
-		themes: [
-			{
-				light: {
-					primary: colors.green[400],
-					'primary-content': colors.white,
-					secondary: colors.cyan[400],
-					'secondary-content': colors.white,
-					accent: colors.lime[400],
-					'accent-content':colors.white,
-					neutral: colors.slate[400],
-					'neutral-content':colors.white,
-					'base-100': colors.gray[50],
-					'base-200': colors.gray[100],
-					'base-300': colors.gray[200],
-					'base-content': colors.gray[900],
-					info: colors.sky[400],
-					'info-content':colors.white,
-					success: colors.green[400],
-					'success-content':colors.white,
-					warning: colors.orange[400],
-					'warning-content':colors.white,
-					error: colors.red[400],
-					'error-content':colors.white,
-				},
-				dark: {
-					primary: colors.emerald[500],
-					'primary-content':colors.white,
-					secondary: colors.cyan[500],
-					'secondary-content':colors.white,
-					accent: colors.lime[500],
-					'accent-content':colors.white,
-					neutral: colors.slate[500],
-					'neutral-content':colors.white,
-					'base-100': colors.gray[950],
-					'base-200': colors.gray[900],
-					'base-300': colors.gray[800],
-					'base-content':colors.white,
-					info: colors.sky[500],
-					'info-content':colors.white,
-					success: colors.green[500],
-					'success-content':colors.white,
-					warning: colors.orange[500],
-					'warning-content':colors.white,
-					error: colors.red[500],
-					'error-content':colors.white,
-				}
-			},
-		],
+		themes: [theme],
 		darkTheme: 'dark',
 		styled: true,
 		base: true,
