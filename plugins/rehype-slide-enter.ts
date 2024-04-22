00374257a4086afebdb87e0c863bd5810d534d36
Avenue-opposites <2949195453@qq.com>
2024-04-22 12:00:45 +0800

    fix: 修复标签style多出一个';'的问题

--- plugins/rehype-slide-enter.ts
+++ plugins/rehype-slide-enter.ts
@@ -3,14 +3,21 @@ import type { Element } from 'hast'
 interface Options {
 	delay?: number | string;
 	maxStagger?: number;
+	className?: string | string[];
 }
 
 export default function rehypeSlideEnter(options: Options = {}) {
 	return (tree: Element) => {
-		const { delay = 60, maxStagger = 20 } = options
+		const { 
+			delay = 60, 
+			maxStagger = 20, 
+			className = ['slide-enter']
+		} = options
+		const c = Array.isArray(className) ? className : [className]
 		const d = typeof delay === 'string' ? delay : delay + 'ms'
 		const children  = tree.children.filter(child => child.type === 'element' || child.type === 'raw')
 		let i = 0
+		
 		while (i < children.length) {
 			let stagger = i
 			if(stagger > maxStagger) {
@@ -19,19 +26,41 @@ export default function rehypeSlideEnter(options: Options = {}) {
 			const child = children[i]
 			
 			if(child.type === 'element') {
-			  const props = child.properties
-			  const { style = '' } = props
-				props.class = (props.class || '') + ' slide-enter'
-		    props.style = style + ` --delay: ${d};--stagger: ${stagger}`
+				const props = child.properties
+				
+				// md ==> className []
+				// mdx ==> class = ''
+				const className = props.className = (props.className || []) as string[]
+				// 把__slide-enter__移除，因为它只在raw中作为占位符
+				const index = className.indexOf('__slide-enter__')
+				if(index !== -1) {
+					className.splice(index, 1)
+				}
+				className.push('slide-enter')
+				props.className = className
+				
+				let { style = '' } = props
+
+				// 把占位符
+				if(child.tagName === 'pre' && typeof style === 'string') {
+					style = style.slice(0, style.length - ';--delay: ?;--stagger: ?'.length)
+				}
+				
+		    props.style = (style && `${style};`) + `--delay: ${d};--stagger: ${stagger}`
+
+				console.log(child.tagName, props)
 			}
 
+			// 处理pre标签
+			// md ==> raw
+			// mdx ==> element
 			if(child.type === 'raw' && child.value.includes('<pre')) {
 				child.value = child.value
-					.replace('__slide-enter__', 'slide-enter')
+					.replace('__slide-enter__', c.join(' '))
 					.replace('?', d)
 					.replace('?', stagger.toString())
 			}
-
+			
 			i++
 		}
 	}
